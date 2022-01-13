5bed77769f862bed02201e64973cc662f8bdf1c1
trongnsiwa <trongnsiwa79@gmail.com>
2022-01-13 20:31:06 +0700

    ass top progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,19 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import '@shared/styles/global.css';
 import { store } from '@redux/store/store';
+import dynamic from 'next/dynamic';
+
+const TopProgressBar = dynamic(
+  () => {
+    return import('@components/top-progress-bar');
+  },
+  { ssr: false }
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <TopProgressBar />
       <Component {...pageProps} />
     </Provider>
   );
